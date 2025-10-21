893124be811680c187cd44bac200fc4257c227c7
iudinov <iudinov@skbkontur.ru>
2025-10-21 16:26:29 +0500

    task 2

--- Tests/TestMongoDatabase.cs
+++ Tests/TestMongoDatabase.cs
@@ -7,8 +7,7 @@ namespace Tests
     {
         public static IMongoDatabase Create()
         {
-            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
-                                        ?? "mongodb://localhost:27017?maxConnecting=100";
+            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING") ?? "mongodb://admin:admin@localhost:27017?maxConnecting=100";
             var mongoClient = new MongoClient(mongoConnectionString);
             return mongoClient.GetDatabase("game-tests");
         }
